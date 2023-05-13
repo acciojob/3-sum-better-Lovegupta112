1666c8af0623a0b58c8554e15f988302b695d876
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-05-13 21:27:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let closest=Number.MAX_SAFE_INTEGER;
+ for(let i=0;i<arr.length-2;i++){
+	let sum=arr[i]+arr[i+1]+arr[i+2];
+	let diff=Math.abs(sum-target);
+	 if(closest>diff){
+		 closest=sum;
+	 }
+ }
+	return closest;
 }
 
 module.exports = threeSum;
