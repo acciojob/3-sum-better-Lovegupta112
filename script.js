61568228b0eb8d9a8434d9d74e51db3cab80ca15
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-22 17:19:44 +0530

    done 3sum

--- script.js
+++ script.js
@@ -10,19 +10,48 @@ function threeSum(arr, target) {
 	// 	}
 	// }
 	// return closest;
-	let closest = Number.MAX_SAFE_INTEGER;
-	for(let i=0;i<arr.length;i++){
-		for(let j=i+1;j<arr.length;j++){
-			for(let k=j+1;k<arr.length;k++){
-             let sum=arr[i]+arr[j]+arr[k];
-			 let diff=Math.abs(target-sum);
-			 if(closest>diff){
-				closest=sum;
-			 }
-			}
+	// brute force O(n^3)---------------------
+	// let closest = Number.MAX_SAFE_INTEGER;
+	// let res=0;
+	// for(let i=0;i<arr.length-2;i++){
+	// 	for(let j=i+1;j<arr.length-1;j++){
+	// 		for(let k=j+1;k<arr.length;k++){
+    //          let sum=arr[i]+arr[j]+arr[k];
+	// 		 let diff=Math.abs(target-sum);
+	// 		 if(closest>diff){
+	// 			console.log(arr[i],arr[j],arr[k]);
+	// 			closest=diff;
+	// 		    res=sum;
+	// 		 }
+	// 		}
+	// 	}
+	// }
+
+	// O(n^2)------------
+	arr.sort();
+      let closestSum = Number.MAX_SAFE_INTEGER;
+	for(let i=0;i<arr.length-2;i++){
+		let j=i+1;
+		let k=arr.length-1;
+
+		while(j<k){
+		let sum=arr[i]+arr[j]+arr[k];
+		if(Math.abs(target-sum)<Math.abs(target-closestSum)){
+         closestSum=sum;
+		}
+		if(sum<target){
+			j++;
+		}
+		else if(sum>target){
+			k--;
+		}else{
+			return closestSum;
+		}
 		}
 	}
-	return closest;
+	// console.log(closestSum);
+	return closestSum;
 }
-
+// threeSum([-1,2,1,-4],1);
+// threeSum([0,1,2],1);
 module.exports = threeSum;
