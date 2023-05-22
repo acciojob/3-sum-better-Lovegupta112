8051e5d8a73d355cbdb19228ce9272f04b5eff68
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-22 15:59:30 +0530

    use bruteforce approach

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 function threeSum(arr, target) {
-// write your code here
-	let closest=Number.MAX_SAFE_INTEGER;
-	// arr=arr.sort();
- for(let i=0;i<arr.length-2;i++){
-	let sum=arr[i]+arr[i+1]+arr[i+2];
-	let diff=Math.abs(sum-target);
-	 if(closest>diff){
-		 closest=sum;
-	 }
- }
+	// write your code here
+	// let closest = Number.MAX_SAFE_INTEGER;
+	// // arr=arr.sort();
+	// for (let i = 0; i < arr.length - 2; i++) {
+	// 	let sum = arr[i] + arr[i + 1] + arr[i + 2];
+	// 	let diff = Math.abs(sum - target);
+	// 	if (closest > diff) {
+	// 		closest = sum;
+	// 	}
+	// }
+	// return closest;
+	let closest = Number.MAX_SAFE_INTEGER;
+	for(let i=0;i<arr.length;i++){
+		for(let j=i+1;j<arr.length;j++){
+			for(let k=j+1;k<arr.length;k++){
+             let sum=arr[i]+arr[j]+arr[k];
+			 let diff=Math.abs(target-sum);
+			 if(closest>diff){
+				closest=sum;
+			 }
+			}
+		}
+	}
 	return closest;
 }
 
